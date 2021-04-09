694568372ba97bed0e188aa5920107daa1c530b5
Joedd <qiaoddvip@126.com>
2021-04-09 13:06:37 +0800

    添加相机

--- ColorThemeToolDemo/ViewController.swift
+++ ColorThemeToolDemo/ViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 
 let kTooBarHeight: CGFloat = 40
 let kPinYinHeight: CGFloat = 26
+let kscreenWidth = UIScreen.main.bounds.size.width
+let kscreenHeight = UIScreen.main.bounds.size.height
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cropImageView: UIImageView!
@@ -18,19 +21,81 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
         test()
         test2()
     }
     
-    func test() {
+    func setupView() {
         if let path = Bundle.main.path(forResource: "image@2x.png", ofType: nil) {
             imageView.image = UIImage(contentsOfFile: path)
         }
-        let size = imageView.frame.size
-        let partRect = CGRect(x: 0, y: kPinYinHeight+10, width: size.width, height: kTooBarHeight-20)
+        
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapAction))
+        imageView.addGestureRecognizer(tapGR)
+    }
+    @objc private func tapAction() {
+        let alertController: UIAlertController = UIAlertController.init(title: "温馨提示", message: nil, preferredStyle: UIAlertController.Style.actionSheet)
+        let action: UIAlertAction = UIAlertAction.init(title: "相机", style: UIAlertAction.Style.default, handler: { (action) in
+            self.choicePhotoWithType(type: .camera)
+            alertController.dismiss(animated: true, completion: nil)
+        })
+        let action1: UIAlertAction = UIAlertAction.init(title: "相册", style: UIAlertAction.Style.default, handler: { (action) in
+            self.choicePhotoWithType(type: .photoLibrary)
+            alertController.dismiss(animated: true, completion: nil)
+        })
+        let action2: UIAlertAction = UIAlertAction.init(title: "关闭", style: UIAlertAction.Style.default, handler: { (action) in
+            alertController.dismiss(animated: true, completion: nil)
+        })
+        alertController.addAction(action)
+        alertController.addAction(action1)
+        alertController.addAction(action2)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func choicePhotoWithType(type: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(type) {
+            let picker = UIImagePickerController.init()
+            picker.delegate = self//代理
+            picker.sourceType = type//来源
+            picker.allowsEditing = false
+
+            self.present(picker, animated: true, completion: nil)
+        }else {
+            let str: String?
+            if type == UIImagePickerController.SourceType.camera {
+                str = "摄像头不可用"
+            }else
+            {
+                str = "相册不可用"
+            }
+            let alertController: UIAlertController = UIAlertController.init(title: "温馨提示", message: str, preferredStyle: UIAlertController.Style.alert)
+            let action: UIAlertAction = UIAlertAction.init(title: "知道了", style: UIAlertAction.Style.default, handler: { (action) in
+                alertController.dismiss(animated: true, completion: nil)
+            })
+            alertController.addAction(action)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image: UIImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        print("\(image)")
+        imageView.image = image
+        picker.dismiss(animated: true, completion: nil)
+        test()
+    }
+    
+    func test() {
+        let partRect = CGRect(x: 0, y: kPinYinHeight, width: kscreenWidth, height: kTooBarHeight)
 
         /*
         if let image = imageView.image {
+            let size = imageView.frame.size
             let scaleRect = CGRect(origin: .zero, size: size)
             
             let color = SMColorThemeTool.mostColor(source: image, scale: scaleRect, part:partRect )
